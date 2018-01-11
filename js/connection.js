e54895f34642cea52a3c73c22a5d8bec00c8b93d
Héctor <hectorri@hotmail.es>
2018-01-11 18:15:40 +0100

    Cambios en indicador de conexión

--- js/connection.js
+++ js/connection.js
@@ -3,8 +3,8 @@ window.addEventListener('offline', updateStatus);
 
 function updateStatus(event) {
 	if(navigator.onLine){
-		document.getElementById('status').innerHTML  = "Conectado";
+		document.getElementById('status').innerHTML  = '<img class="img__status" src="../images/connected.png"/>Conectado';
 	}else{
-		document.getElementById('status').innerHTML = "Desconectado";
+		document.getElementById('status').innerHTML = '<img class="img__status" src="../images/disconnected.png"/>Desconectado';
 	}
 }
